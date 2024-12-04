5ef36d9032eab9bb2bed79c552300b9de5a7864d
Muhammed Favas <favas78601@gmail.com>
2024-12-04 23:08:11 +0530

    complete random password generator

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const passwordBox = document.getElementById("password")
-const length = 12;
+const lenght = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerCase = "abccdefghijklmnopqrstuvwxyz"
 const number = "0123456789"
 const symbol = "!@#$%^&*()_+=-\{}][/<>"
 
+const allChars = upperCase + lowerCase + number + symbol;
 
-function createPassword (){
-    let password = ""
-    password += upperCase[Math.floor(math.random()*upperCase.length)]
-    password += loeweCase[Math.floor(math.random()*lowerCase.length)]
-    password += number[Math.floor(math.random()*number.length)]
-    password += symbol[Math.floor(math.random()*symbol.length)]
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)]
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+    password += number[Math.floor(Math.random() * number.length)]
+    password += symbol[Math.floor(Math.random() * symbol.length)]
+
+    while(lenght > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)]
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
 }
